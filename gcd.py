d2f2f3f970aba735b0e03ec1c2f0597fa8948584
CarolynMillMoo <G00398299@gmit.ie>
2021-10-09 23:00:52 +0100

    adding in changes following the functions review video

--- gcd.py
+++ gcd.py
@@ -1,9 +1,17 @@
 # While loops tutorial
 # Author Carolyn Moorhouse
-a = 50
-b = 20
+# Altered to include function review
+def gcd(a, b):
+  """
+  Returns the greatest common divisor of a and b.
+  """
+  if a < b:
+      a, b = b, a
+      
+  while b > 0:
+     a, b = b, a % b
 
-while b > 0:
-    a, b = b, a % b
+  return a
 
-print(a)
+print(gcd(50, 20))
+print(gcd(22, 143))
